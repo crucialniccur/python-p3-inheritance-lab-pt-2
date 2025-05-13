845a99b69527d8648dcc46e3b732d089656f1cae
Mati Joseph <crucialniccur@gmail.com>
2025-05-13 21:24:34 +0300

    call super 10 times

--- lib/student.py
+++ lib/student.py
@@ -17,3 +17,8 @@ class ChattyStudent(Student):
     def hello(self):
         super().hello()
         print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    # 2. Write a method in the ChattyStudent class, raise_hand(), that uses super() ten times so that the method will print() out "Pick me!" ten times. It is possible to call super() multiple times in a method.
+    def raise_hand(self):
+        for _ in range(10):
+            super().raise_hand()
