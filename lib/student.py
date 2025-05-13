b70926b917ac453b8c46fb233c7399ad24df3c6a
Mati Joseph <crucialniccur@gmail.com>
2025-05-13 21:15:56 +0300

    define raise_hand method in Student

--- lib/student.py
+++ lib/student.py
@@ -1,7 +1,13 @@
-#  Write a method in the Student class, hello(), that print()s out the phrase :"Hey there! I'm so excited to learn stuff."
+# 1. Write a method in the Student class, hello(), that print()s out the phrase :"Hey there! I'm so excited to learn stuff."
+
+# 2. Write a method in the Student class, raise_hand(), that print()s out the phrase: "Pick me!"
+
 class Student:
     def hello(self):
         print("Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        print('Pick me!')
 
 
 class ChattyStudent(Student):
