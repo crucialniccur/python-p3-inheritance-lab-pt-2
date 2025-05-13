ea2d33a9bef52010f33f364c86fa723b8b25f5f0
Mati Joseph <crucialniccur@gmail.com>
2025-05-13 21:19:32 +0300

    define hello in ChattyStudent

--- lib/student.py
+++ lib/student.py
@@ -10,5 +10,10 @@ class Student:
         print('Pick me!')
 
 
+# 1. Write a method in the ChattyStudent class, hello(), that uses super() to inherit the behavior of the hello() method from the parent class. Then, augment that method to also print() out the very chatty phrase:
+# "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
