a714b2aeaa31318e7cc3af78236c04792dd43f6f
Mati Joseph <crucialniccur@gmail.com>
2025-05-13 21:14:13 +0300

    define the hello method

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,8 @@
+#  Write a method in the Student class, hello(), that print()s out the phrase :"Hey there! I'm so excited to learn stuff."
 class Student:
-    pass
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+
 
 class ChattyStudent(Student):
     pass
